b93ecb25faf9ddaaedc8772a9d9731e63fd1dc66
Kevin Schaul <kevin.schaul@gmail.com>
2013-08-06 18:07:54 -0400

    Captures preset portion of a preset page

--- src/depict.js
+++ src/depict.js
@@ -3,6 +3,7 @@
 var child_process = require('child_process');
 var fs = require('fs');
 var optimist = require('optimist');
+var phantom = require('phantom');
 
 var argv = optimist
   .usage('Usage: depict URL OUT_FILE [OPTIONS]')
@@ -48,6 +49,21 @@ if (hide_selector) {
   css_text += "\n\n " + hide_selector + " { display: none; }\n";
 }
 
+phantom.create(function(ph) {
+  ph.createPage(function(page) {
+    page.open('http://www.nytimes.com/interactive/2013/07/07/business/a-nation-of-wineries.html',
+        function(status) {
+      console.log(this);
+      console.log(status);
+      page.set('clipRect', { top: 200, left: 10, width: 950, height: 600 });
+      page.render('out.png', function() {
+        console.log('done!');
+      });
+    });
+  });
+});
+
+/*
 var args = [__dirname + '/depict-casperjs.js', url, selector, out_file];
 
 if (css_text) {
@@ -59,4 +75,5 @@ casper = child_process.spawn('casperjs', args);
 casper.stdout.on('data', function(data) {
     process.stdout.write(data);
 });
+*/
 
